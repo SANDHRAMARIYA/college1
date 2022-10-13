414e84f96ae222943810eb32dc73dcd36c024c17
SANDHRAMARIYA <sandhramariya09@gmail.com>
2022-10-14 00:52:45 +0530

    form

--- src/app/addstudent/addstudent.component.ts
+++ src/app/addstudent/addstudent.component.ts
@@ -9,6 +9,32 @@ export class AddstudentComponent implements OnInit {
 
   constructor() { }
 
+  name=""
+  collegename=""
+  parentname=""
+  dob=""
+  adress=""
+  mobilenum=""
+  parntnum=""
+  email=""
+  blood=""
+
+
+  readValues=()=>{
+    let data={
+      "name":this.name,
+      "collegename":this.collegename,
+      "parentname":this.collegename,
+      "dob":this.dob,
+      "adress":this.adress,
+      "mobilenum":this.mobilenum,
+      "parntnum":this.parntnum,
+      "email":this.email,
+      "blood":this.blood
+    }
+    console.log(data)
+  }
+
   ngOnInit(): void {
   }
 
